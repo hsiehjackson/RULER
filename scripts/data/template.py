52a99bdceaa53b581c38842842c4ae81ec197020
LU <792823239@qq.com>
2024-08-09 11:55:00 +0800

    Add the template of Phi3 in template.py

--- scripts/data/template.py
+++ scripts/data/template.py
@@ -27,5 +27,5 @@ Templates = {
     
     'RWKV': "User: hi\n\nAssistant: Hi. I am your assistant and I will provide expert full response in full details. Please feel free to ask any question and I will always answer it\n\nUser: {task_template}\n\nAssistant:",
 
-    'Phi3': "<|user|> {task_template} <|end|> <|assistant|>",
+    'Phi3': "<|user|>\n{task_template}<|end|>\n<|assistant|>\n",
 }
